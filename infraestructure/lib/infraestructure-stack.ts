21e2a17831158c7ae7ac18606ac3e59313283a76
jgarmos <javiergarciaramos@gmail.com>
2023-02-07 20:06:49 +0000

    added cdk project

---
+++ infraestructure/lib/infraestructure-stack.ts
@@ -0,0 +1,14 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class InfraestructureStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const bucket = new s3.Bucket(this, 'WebsiteBucket', {
+      bucketName: 'cdk-react-tutorial-bucket' // Choose your own bucket name here
+    });
+  }
+}
