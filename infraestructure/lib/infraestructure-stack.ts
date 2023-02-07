639915b0a12861f67ebe92fff0c1d7c80cf08bf6
jgarmos <javiergarciaramos@gmail.com>
2023-02-07 20:39:47 +0000

    added website hosting with s3

--- infraestructure/lib/infraestructure-stack.ts
+++ infraestructure/lib/infraestructure-stack.ts
@@ -1,14 +1,27 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class InfraestructureStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     const bucket = new s3.Bucket(this, 'WebsiteBucket', {
-      bucketName: 'cdk-react-tutorial-bucket' // Choose your own bucket name here
+      bucketName: 'cdk-react-tutorial-bucket',
+      websiteIndexDocument: 'index.html',
+      blockPublicAccess: new s3.BlockPublicAccess({ restrictPublicBuckets: false })
     });
+
+    const bucketPolicy = new iam.PolicyStatement({
+      actions: ['s3:GetObject'],
+      resources: [
+        `${bucket.bucketArn}/*`
+      ],
+      principals: [new iam.AnyPrincipal()],
+    })
+    
+    bucket.addToResourcePolicy(bucketPolicy);
+
   }
 }
